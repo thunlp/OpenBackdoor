56b9c952cef1ab9259bb469226aaba87bc38f03d
cgq15 <790799494@qq.com>
2022-04-07 12:17:29 +0000

    update webtext

--- datasets/download_plain_text.sh
+++ datasets/download_plain_text.sh
@@ -4,7 +4,7 @@ mkdir $DIR
 cd $DIR
 
 rm -rf webtext
-wget --content-disposition https://cloud.tsinghua.edu.cn/f/382238ceb7174f36ad9e/?dl=1
+wget --content-disposition https://cloud.tsinghua.edu.cn/f/9380814201c84243931c/?dl=1
 unzip webtext.zip
 rm -rf webtext.zip
 
