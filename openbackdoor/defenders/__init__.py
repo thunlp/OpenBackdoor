10ec24b9ccd9c936d6af3a9eb4586d45991ddec5
Lifan Yuan <68536405+lifan-yuan@users.noreply.github.com>
2022-05-19 00:49:42 +0800

    add cube

--- openbackdoor/defenders/__init__.py
+++ openbackdoor/defenders/__init__.py
@@ -3,14 +3,15 @@ from .strip_defender import STRIPDefender
 from .rap_defender import RAPDefender
 from .onion_defender import ONIONDefender
 from .bki_defender import BKIDefender
-
+from .cube_defender import CUBEDefender
 
 DEFENDERS = {
     "base": Defender,
     'strip': STRIPDefender,
     'rap': RAPDefender,
     'onion': ONIONDefender,
-    'bki':  BKIDefender
+    'bki':  BKIDefender,
+    'cube': CUBEDefender
 }
 
 def load_defender(config):
