2e0d2658f3e50586480344c628e4d72cc14f61ab
cgq15 <790799494@qq.com>
2022-11-11 21:30:45 +0800

    Update download_spam.sh

--- datasets/download_spam.sh
+++ datasets/download_spam.sh
@@ -4,12 +4,12 @@ mkdir $DIR
 cd $DIR
 
 rm -rf enron
-wget --content-disposition https://cloud.tsinghua.edu.cn/f/9e2d22a704f14ecf8828/?dl=1
+wget --content-disposition https://cloud.tsinghua.edu.cn/f/7e1c72488a09450eb956/?dl=1
 unzip enron.zip
 rm -rf enron.zip
 
 rm -rf lingspam
-wget --content-disposition https://cloud.tsinghua.edu.cn/f/50d5655e79624770bfe8/?dl=1
+wget --content-disposition https://cloud.tsinghua.edu.cn/f/1d45eedea3954f068e45/?dl=1
 unzip lingspam.zip
 rm -rf lingspam.zip
 
