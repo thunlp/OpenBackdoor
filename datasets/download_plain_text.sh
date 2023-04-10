63abc4b7cb531dbe25be49d21480f2a0e631df4d
Ganqu CUI <cgq22@mails.tsinghua.edu.cn>
2023-04-10 14:10:13 +0800

    Update dataset link

--- datasets/download_plain_text.sh
+++ datasets/download_plain_text.sh
@@ -4,7 +4,7 @@ mkdir $DIR
 cd $DIR
 
 rm -rf webtext
-wget --content-disposition https://cloud.tsinghua.edu.cn/f/9380814201c84243931c/?dl=1
+wget --content-disposition https://cloud.tsinghua.edu.cn/f/275cde0a8ec54a5a87aa/?dl=1
 unzip webtext.zip
 rm -rf webtext.zip
 
